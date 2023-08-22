495f434a17ebb69dc86eca7f89336cb3ec143a4b
Abubakar30497 <98089653+Abubakar30497@users.noreply.github.com>
2023-08-22 12:08:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 import requests
-
+from bs4 import BeautifulSoup
 import os
 import pandas as pd
 import numpy as np
