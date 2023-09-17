d0c465cdc700f86f7fc2bf73f9fc6cc89d940656
Abubakar30497 <98089653+Abubakar30497@users.noreply.github.com>
2023-09-17 15:04:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -287,7 +287,7 @@ extracted_text = textscrapper(query_text)
 
 st.subheader("Summary:")
 sums = []
-for text in extracted_text:
+for text in extracted_text[::-1]:
     sums.append(summarize(text))
 mediate_summary = ''
 for s in sums:
